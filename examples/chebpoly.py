2b711ab21f9adc003d8c944bc1e1654738187182
Olivier Verdier <olivier.verdier@gmail.com>
2012-04-02 12:45:49 +0200

    add example with chebpoly
    plot the first N Chebyshev polynomials

---
+++ examples/chebpoly.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: UTF-8
+from __future__ import division
+
+from pychebfun import *
+
+
+def plot_first_chebyshev(N, alpha_coeff=10):
+    """
+    Plot the N first Chebyshev polynomials.
+
+    alpha_coeff:  how slowly higher order polynomial fade away in the plot
+    """
+    cs = [chebpoly(deg) for deg in range(N)]
+    for k,c in enumerate(cs):
+        c.plot(color='black', alpha=alpha_coeff/(k+alpha_coeff))
+    return cs
+
+plot_first_chebyshev(50)
