8d7b15d5ee4b310294433bbd1dc527b1ee4f3d15
Olivier Verdier <olivier.verdier@gmail.com>
2012-04-02 12:33:28 +0200

    add example with approximation of abs
    shows when the dichotomy algorithm is close not to converge

---
+++ examples/abs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: UTF-8
+from __future__ import division
+"""
+This example shows the limits of Chebfun without splitting.
+The function abs may be represented with four interpolation points as a piecewise Chebfun.
+Instead, the dichotomy algorithm on [-1,1] produces more and more points, and does not even converge.
+"""
+
+from pychebfun import *
+import numpy as np
+np.seterr(all='raise')
+
+def abse(x, epsilon):
+    """
+    Smooth approximation of the abs function.
+    """
+    return np.sqrt(epsilon + x**2)
+
+import functools
+
+abses = [functools.partial(abse, epsilon=pow(10,-k)) for k in range(4)]
+cs = []
+for f in abses:
+    try:
+        c = Chebfun(f)
+    except c.NoConvergence:
+        break
+    else:
+        cs.append(c)
+
+c,f = zip(cs,abses)[3]
+c.compare(f)
