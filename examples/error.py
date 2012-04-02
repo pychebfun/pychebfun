ded188910d7add96f1ad51d17c11e51f6b0a487c
Olivier Verdier <olivier.verdier@gmail.com>
2012-04-02 14:31:16 +0200

    use Chebfun.compare in example/error.py

--- examples/error.py
+++ examples/error.py
@@ -10,16 +10,4 @@ x = linspace(-1,1,1000)
 # Constuct the O(dx^-16) "spectrally accurate" chebfun p and compute the error
 # between p and f at each point in the domain
 p   = Chebfun(f)
-err = abs(f(x)-p(x))
-
-
-# Plot the functions o top of each other and the abs error at each point
-figure(1)
-subplot(2,1,1)
-plot(x,f(x),'k',linewidth=10,alpha=0.3)
-plot(x,f(x),'r',linewidth=3)
-
-subplot(2,1,2)
-plot(x,err,'k')
-
-show()
+p.compare(f)
