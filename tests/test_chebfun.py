9d7be666142f36bc745803c7e2ce9ae18b0f127b
Olivier Verdier <olivier.verdier@gmail.com>
2012-04-12 13:21:35 +0200

    tests compatible with unittest
    tests may now be run with or without nose

--- tests/test_chebfun.py
+++ tests/test_chebfun.py
@@ -8,16 +8,28 @@ matplotlib.use('agg')
 
 import os
 
+import sys
+testdir = os.path.dirname(__file__)
+moduledir = os.path.join(testdir, os.path.pardir)
+sys.path.insert(0, moduledir)
 from pychebfun import *
 
 import numpy as np
 np.seterr(all='raise')
 import numpy.testing as npt
-import nose.tools as nt
+
+import unittest
 
 
 def f(x):
     return np.sin(6*x) + np.sin(30*np.exp(x))
+
+
+def fd(x):
+    """
+    Derivative of f
+    """
+    return 6*np.cos(6*x) + np.cos(30*np.exp(x))*30*np.exp(x)
 
 def runge(x):
     return 1./(1+25*x**2)
@@ -28,14 +40,14 @@ def zero(x):
 
 xs = np.linspace(-1,1,1000)
 
-class Test_Chebfun(object):
+class Test_Chebfun(unittest.TestCase):
     def setUp(self):
         # Constuct the O(dx^-16) "spectrally accurate" chebfun p
         Chebfun.record = True
         self.p = Chebfun(f,)
 
     def test_len(self):
-        nt.assert_equal(len(self.p), len(self.p.chebyshev_coefficients()))
+        self.assertEqual(len(self.p), len(self.p.chebyshev_coefficients()))
 
     def test_error(self):
         x = xs
@@ -53,11 +65,11 @@ class Test_Chebfun(object):
         plt.clf()
         self.p.plot()
         a = plt.gca()
-        nt.assert_equal(len(a.lines),2)
+        self.assertEqual(len(a.lines),2)
         plt.clf()
         self.p.plot(interpolation_points=False)
         a = plt.gca()
-        nt.assert_equal(len(a.lines),1)
+        self.assertEqual(len(a.lines),1)
 
     def test_cheb_plot(self):
         self.p.compare(f)
@@ -88,36 +100,36 @@ class Test_Chebfun(object):
         N = 32
         data = np.random.rand(N+1)
         even = self.p.even_data(data)
-        nt.assert_equal(len(even), 2*N)
+        self.assertEqual(len(even), 2*N)
 
     def test_N(self):
         N = len(self.p) - 1
         pN = Chebfun(f, N)
-        nt.assert_equal(len(pN.chebyshev_coefficients()), N+1)
-        nt.assert_equal(len(pN.chebyshev_coefficients()),len(pN))
+        self.assertEqual(len(pN.chebyshev_coefficients()), N+1)
+        self.assertEqual(len(pN.chebyshev_coefficients()),len(pN))
         npt.assert_array_almost_equal(pN(xs), self.p(xs))
         npt.assert_array_almost_equal(pN.chebyshev_coefficients(),self.p.chebyshev_coefficients())
 
     def test_record(self):
         p = Chebfun(f)
-        nt.assert_equal(len(p.bnds), 7)
+        self.assertEqual(len(p.bnds), 7)
 
     def test_zero(self):
         p = Chebfun(zero)
-        nt.assert_equal(len(p),5) # should be equal to the minimum length, 4+1
+        self.assertEqual(len(p),5) # should be equal to the minimum length, 4+1
 
 
     def test_nonzero(self):
-        nt.assert_true(self.p)
+        self.assertTrue(self.p)
         mp = Chebfun(zero)
-        nt.assert_false(mp)
+        self.assertFalse(mp)
 
     def test_integral(self):
         def q(x):
             return x*x
         p = Chebfun(q)
         i = p.integral()
-        nt.assert_almost_equal(i,2/3)
+        self.assertAlmostEqual(i,2/3)
 
     def test_interp_values(self):
         """
@@ -128,82 +140,90 @@ class Test_Chebfun(object):
         npt.assert_array_almost_equal(self.p(xs), p2(xs))
 
     def test_equal(self):
-        nt.assert_equal(self.p, Chebfun(self.p))
+        self.assertEqual(self.p, Chebfun(self.p))
 
-def test_truncate(N=17):
-    """
-    Check that the Chebyshev coefficients are properly truncated.
-    """
-    small = Chebfun(f, N=N)
-    new = Chebfun(small)
-    nt.assert_equal(len(new), len(small),)
+class Test_Misc(unittest.TestCase):
+    def test_truncate(self, N=17):
+        """
+        Check that the Chebyshev coefficients are properly truncated.
+        """
+        small = Chebfun(f, N=N)
+        new = Chebfun(small)
+        self.assertEqual(len(new), len(small),)
 
-def test_error():
-    chebpolyplot(f)
+    def test_error(self):
+        chebpolyplot(f)
 
-def test_vectorized():
-    fv = np.vectorize(f)
-    p = Chebfun(fv)
+    def test_vectorized(self):
+        fv = np.vectorize(f)
+        p = Chebfun(fv)
 
-def test_examples():
-    """
-    Check that the examples can be executed.
-    """
-    here = os.path.dirname(__file__)
-    example_folder = os.path.join(here,os.path.pardir,'examples')
-    files = os.listdir(example_folder)
-    for example in files:
-        file_name = os.path.join(example_folder,example)
-        try:
-            execfile(file_name, {})
-        except Exception as e:
-            raise Exception('Error in {0}: {0}'.format(example), e)
+    def test_examples(self):
+        """
+        Check that the examples can be executed.
+        """
+        here = os.path.dirname(__file__)
+        example_folder = os.path.join(here,os.path.pardir,'examples')
+        files = os.listdir(example_folder)
+        for example in files:
+            file_name = os.path.join(example_folder,example)
+            try:
+                execfile(file_name, {})
+            except Exception as e:
+                raise Exception('Error in {0}: {0}'.format(example), e)
 
-def test_chebpoly(ns=[0,5]):
-    for n in ns:
-        c = chebpoly(n)
-        npt.assert_array_almost_equal(c.chebyshev_coefficients(), [0]*n+[1.])
+    def test_chebpoly(self, ns=[0,5]):
+        for n in ns:
+            c = chebpoly(n)
+            npt.assert_array_almost_equal(c.chebyshev_coefficients(), [0]*n+[1.])
 
-def test_list_init():
-    c = Chebfun([1.])
-    npt.assert_array_almost_equal(c.chebyshev_coefficients(),[1.])
+    def test_list_init(self):
+        c = Chebfun([1.])
+        npt.assert_array_almost_equal(c.chebyshev_coefficients(),[1.])
 
-def test_scalar_init():
-    one = Chebfun(1.)
-    npt.assert_array_almost_equal(one(xs), 1.)
+    def test_scalar_init(self):
+        one = Chebfun(1.)
+        npt.assert_array_almost_equal(one(xs), 1.)
 
-@nt.raises(Chebfun.NoConvergence)
-def test_no_convergence():
-    Chebfun(np.sign)
+    def test_no_convergence(self):
+        with self.assertRaises(Chebfun.NoConvergence):
+            Chebfun(np.sign)
 
-class Test_Arithmetic(object):
+    def test_runge(self):
+        """
+        Test some of the capabilities of operator overloading.
+        """
+        r = Chebfun(runge)
+        x = chebpoly(1)
+        rr = 1./(1+25*x**2)
+        npt.assert_almost_equal(r(xs),rr(xs), decimal=13)
+
+
+
+class Test_Arithmetic(unittest.TestCase):
     def setUp(self):
         self.p1 = Chebfun(f)
         self.p2 = Chebfun(runge)
 
     def test_scalar_mul(self):
-        nt.assert_equal(self.p1, self.p1)
-        nt.assert_equal(self.p1*1, 1*self.p1)
-        nt.assert_equal(self.p1*1, self.p1)
-        nt.assert_equal(0*self.p1, zero)
+        self.assertEqual(self.p1, self.p1)
+        self.assertEqual(self.p1*1, 1*self.p1)
+        self.assertEqual(self.p1*1, self.p1)
+        self.assertEqual(0*self.p1, zero)
 
     def test_commutativity(self):
-        nt.assert_equal(self.p1*self.p2, self.p2*self.p1)
-        nt.assert_equal(self.p1+self.p2, self.p2+self.p1)
+        self.assertEqual(self.p1*self.p2, self.p2*self.p1)
+        self.assertEqual(self.p1+self.p2, self.p2+self.p1)
 
     def test_minus(self):
         a = self.p1 - self.p2
         b = self.p2 - self.p1
-        nt.assert_equal(a+b,0)
+        self.assertEqual(a+b,0)
 
 
 
-def test_runge():
-    """
-    Test some of the capabilities of operator overloading.
-    """
-    r = Chebfun(runge)
-    x = chebpoly(1)
-    rr = 1./(1+25*x**2)
-    npt.assert_almost_equal(r(xs),rr(xs), decimal=13)
-
+if __name__ == '__main__':
+    unittest.main()
+    ## suite = unittest.TestLoader().loadTestsFromTestCase(Test_Chebfun)
+    ## unittest.TextTestRunner(verbosity=2).run(suite)
+    
