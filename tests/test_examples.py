5f7e9c06e8c1b787054f11ffc6afbddb0b008a3f
Olivier Verdier <olivier.verdier@gmail.com>
2015-01-04 12:34:18 +0100

    move test_examples in separate file

---
+++ tests/test_examples.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: UTF-8
+from __future__ import division
+
+import os
+import unittest
+
+class TestFunctional(unittest.TestCase):
+    def test_examples(self):
+        """
+        Check that the examples can be executed.
+        """
+        here = os.path.dirname(__file__)
+        example_folder = os.path.join(here,os.path.pardir,'examples')
+        files = os.listdir(example_folder)
+        for example in files:
+            file_name = os.path.join(example_folder,example)
+            try:
+                execfile(file_name, {})
+            except Exception as e:
+                raise Exception('Error in {0}: {0}'.format(example), e)
+
